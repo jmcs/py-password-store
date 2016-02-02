f51b4f411d4e9d8fb2be14a9a38f45facd87a709
João Santos <jmcs@jsantos.eu>
2016-02-02 21:02:14 +0100

    First version

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+# TODO add README and long_description
+
+setup(
+    name='password-store',
+    packages=find_packages(),
+    version=version,
+    description='Password Store Wrapper',
+    author='João Santos',
+    author_email='jmcs@jsantos.eu',
+    url='https://github.com/jmcs/py-password-store',
+    license='MIT License',
+    install_requires=['pyyaml'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+    ],
+)
